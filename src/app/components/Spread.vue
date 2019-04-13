8eb45604f2f84bed93e80d7607b398b86935fb64
Masaharu Komuro <komuro-m1@asahi.com>
2019-04-14 02:42:25 +0900

    不要な記述を削除

--- src/app/components/Spread.vue
+++ src/app/components/Spread.vue
@@ -65,111 +65,27 @@
 
 <script lang="ts">
   import { Component, Prop, Watch } from "nuxt-property-decorator";
-  import { email, required } from "vuelidate/lib/validators";
   import { MyVue } from "~/types/types";
   import { Getter } from "vuex-class";
 
-  @(Component as any)( {
-    validations: {
-      account:  { required },
-      email:    { required, email },
-      select:   { required },
-      checkbox: {
-        checked( val ) {
-          return val;
-        }
-      }
-    },
-    data() {
-      return {
-        // form variables
-
-        dialog:        false,
-        dialog2:       false,
-        dialog3:       false,
-        notifications: false,
-        sound:         true,
-        widgets:       false,
-        items:         [
-          {
-            title: "Click Me"
-          },
-          {
-            title: "Click Me"
-          },
-          {
-            title: "Click Me"
-          },
-          {
-            title: "Click Me 2"
-          }
-        ],
-        select:        [
-          { text: "State 1" },
-          { text: "State 2" },
-          { text: "State 3" },
-          { text: "State 4" },
-          { text: "State 5" },
-          { text: "State 6" },
-          { text: "State 7" }
-        ]
-      };
-    }
-
-  } )
+  @Component( {} )
   export default class Spread extends MyVue {
     @Getter( "getAccount" ) getAccount;
 
     @Prop() post!: string;
 
+    dialog: boolean = false;
+
     isAccount: boolean = false;
 
     mounted() {
+      this.isAccount = !!this.getAccount;
     }
 
     @Watch( "getAccount" )
     onAccountChange( _new ) {
       this.isAccount = !!_new;
     };
-
-    get accountErrors() {
-      const errors = [];
-      console.log( "name" );
-      console.log( this.$v.name );
-      !(this as any).$v.account.required && errors.push( "Name is required." );
-      return errors;
-    };
-
-    checkboxErrors() {
-      const errors = [];
-      if ( !(this as any).$v.checkbox.$dirty ) return errors;
-      !(this as any).$v.checkbox.checked && errors.push( "You must agree to continue!" );
-      console.log( errors );
-      return errors;
-    };
-
-    emailErrors() {
-      const errors = [ "" ];
-      if ( !(this as any).$v.email.$dirty ) return errors;
-      !(this as any).$v.email.email && errors.push( "Must be valid e-mail" );
-      !(this as any).$v.email.required && errors.push( "E-mail is required" );
-
-      console.log( "error" );
-      console.log( errors );
-      return errors;
-    };
-
-    submit() {
-      (this as any).$v.$touch();
-    };
-
-    clear() {
-      (this as any).$v.$reset();
-      (this as any).name     = "";
-      (this as any).email    = "";
-      (this as any).select   = null;
-      (this as any).checkbox = false;
-    }
 
   }
 </script>
